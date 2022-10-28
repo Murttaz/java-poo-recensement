c1fada7e7f310b8da4ca14a7b00f5d4e505c1992
Murttaz <vincentfournier1993@gmail.com>
2022-10-28 15:09:36 +0200

    Rendu Tp gestion exception

---
+++ src/main/java/fr/diginamic/recensement/services/APPException.java
@@ -0,0 +1,13 @@
+package fr.diginamic.recensement.services;
+
+
+public class APPException extends Exception {
+
+	public APPException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+}
--- src/main/java/fr/diginamic/recensement/services/RecherchePopulationBorneService.java
+++ src/main/java/fr/diginamic/recensement/services/RecherchePopulationBorneService.java
@@ -3,6 +3,8 @@ package fr.diginamic.recensement.services;
 import java.util.List;
 import java.util.Scanner;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 import fr.diginamic.recensement.entites.Recensement;
 import fr.diginamic.recensement.entites.Ville;
 
@@ -17,27 +19,44 @@ import fr.diginamic.recensement.entites.Ville;
 public class RecherchePopulationBorneService extends MenuService {
 
 	@Override
-	public void traiter(Recensement rec, Scanner scanner) {
+	public void traiter(Recensement rec, Scanner scanner) throws APPException {
 
 		System.out.println("Quel est le code du département recherché ? ");
 		String choix = scanner.nextLine();
 
 		System.out.println("Choississez une population minimum (en milliers d'habitants): ");
 		String saisieMin = scanner.nextLine();
-		
+		if (!NumberUtils.isDigits(saisieMin)) {
+			throw new APPException("Veuillez ne remplir qu'avec des chiffres.");
+		}
 		System.out.println("Choississez une population maximum (en milliers d'habitants): ");
 		String saisieMax = scanner.nextLine();
+		if (!NumberUtils.isDigits(saisieMax)) {
+			throw new APPException("Veuillez ne remplir qu'avec des chiffres.");
+		}
 
 		int min = Integer.parseInt(saisieMin) * 1000;
 		int max = Integer.parseInt(saisieMax) * 1000;
 		
+		
+		if (min<0 || max<0 || min>max) {
+			throw new APPException("les minimum et maximum ne peuvent pas être négaifs et le minima ne peut pas être supérieur au maxima.");
+		}
+		
+		
+		boolean departementtrouve=false;
 		List<Ville> villes = rec.getVilles();
 		for (Ville ville : villes) {
 			if (ville.getCodeDepartement().equalsIgnoreCase(choix)) {
+				departementtrouve=true;
 				if (ville.getPopulation() >= min && ville.getPopulation() <= max) {
 					System.out.println(ville);
 				}
 			}
+		}
+		
+		if(!departementtrouve) {
+			throw new APPException("mercide renseigner un code de département existant.");
 		}
 	}
 
